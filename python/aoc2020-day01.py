93b20a4d16a82e5b12e83e70ac85e4bda0ff630d
Daniël van Adrichem <daniel5gh@spiet.nl>
2021-11-30 21:51:43 +0100

    clean up

--- python/aoc2020-day01.py
+++ python/aoc2020-day01.py
@@ -1,29 +1,25 @@
 import itertools
-import logging
-
-__author__ = 'Daniël'
 
 import common
 
-
-_log = logging.getLogger(__name__)
-print = _log.info
-
+__author__ = 'Daniël'
 input = list(map(int, common.read_input('01', 2020)))
+
 
 def part1():
     for x, y in itertools.permutations(input, r=2):
         if x+y == 2020:
             print(x*y)
+            break
 
 
 def part2():
     for x, y, z in itertools.permutations(input, r=3):
         if x+y+z == 2020:
             print(x*y*z)
+            break
 
 
 if __name__=='__main__':
-    logging.basicConfig(level=logging.DEBUG)
     part1()
     part2()
