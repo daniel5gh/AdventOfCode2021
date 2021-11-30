f0bc3f335665ebb78bf81f8b6c6d725e92f05326
Daniël van Adrichem <daniel5gh@spiet.nl>
2021-11-30 21:53:05 +0100

    prepare 2021 day 01

---
+++ python/aoc2021-day01.py
@@ -0,0 +1,19 @@
+import itertools
+
+import common
+
+__author__ = 'Daniël'
+input = list(map(int, common.read_input('01')))
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+if __name__=='__main__':
+    part1()
+    part2()
