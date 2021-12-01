e9d5dfe8aa5975079b44583efffd879d5c8b9f1b
Daniël van Adrichem <daniel5gh@spiet.nl>
2021-12-01 06:35:16 +0100

    day 01 python

--- python/aoc2021-day01.py
+++ python/aoc2021-day01.py
@@ -1,5 +1,4 @@
 import itertools
-
 import common
 
 __author__ = 'Daniël'
@@ -7,13 +6,31 @@ input = list(map(int, common.read_input('01')))
 
 
 def part1():
-    pass
-
+    c = 0
+    p_d = max(input)
+    for d in input:
+        if d > p_d:
+            c += 1
+        p_d = d
+    print(c)
 
 def part2():
-    pass
+    c = 0
+    a = 0
+    p_d = max(input) * 3
+    for d in range(len(input)):
+        w = input[d-3:d]
+        if len(w) != 3:
+            continue
+        d = sum(w)
+        if d > p_d:
+            c += 1
+        a += 1
+        p_d = d
+    print(c+1, a) # bug! a should be 1998, but is 1997 so I guessed the +1 which was correct
 
 
 if __name__=='__main__':
+    print(len(input))
     part1()
     part2()
