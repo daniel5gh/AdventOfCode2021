bbfb6e3454d78161947d883129281377ee51e9b9
Daniël van Adrichem <daniel5gh@spiet.nl>
2021-12-06 22:49:17 +0100

    prepare day 07

---
+++ python/aoc2021-day07.py
@@ -0,0 +1,21 @@
+import common
+from collections import defaultdict
+
+__author__ = 'Daniël'
+
+input = common.read_input('07')
+input = common.read_input('07test')
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+if __name__ == '__main__':
+    print(len(input))
+    part1()
+    part2()
