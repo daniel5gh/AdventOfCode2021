b3b39bc21ba3f2449228391b84ebbb174b062b54
Daniël van Adrichem <daniel5gh@spiet.nl>
2021-12-07 10:27:14 +0100

    day 07

--- python/aoc2021-day07.py
+++ python/aoc2021-day07.py
@@ -1,18 +1,42 @@
 import common
 from collections import defaultdict
+import math
 
 __author__ = 'Daniël'
 
 input = common.read_input('07')
-input = common.read_input('07test')
+# input = common.read_input('07test')
+pos = list(map(int, input[0].split(',')))
 
 
 def part1():
-    pass
+    f = 0
+    for p in pos:
+        # 362 median
+        f += math.fabs(362 - p)
+    print(f)
 
 
 def part2():
-    pass
+    cheap = 1e100
+    actual_mid = 1e100
+    # 500 guess, turned out 499!
+    for mid in range(500):
+        f = 0
+        for p in pos:
+            c = 0
+            # guess
+            # steps = int(math.fabs(math.floor(df.std()) - p))
+            steps = int(math.fabs(mid - p))
+            # for i in range(steps + 1):
+            #     c += i
+            c = steps * (steps + 1) / 2
+            f += c
+        old_cheap = cheap
+        cheap = min(cheap, f)
+        if old_cheap != cheap:
+            actual_mid = mid
+    print(cheap, actual_mid)
 
 
 if __name__ == '__main__':
